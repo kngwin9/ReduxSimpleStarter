69e6d2c8ec23e0449c6cc140c34e03292d9faaa9
Kevin Nguyen <kngwin9@gmail.com>
2017-04-06 11:06:15 -0700

    Saving

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+// Youtube API Key
+const API_KEY = "AIzaSyAe-u8ixNnyeY8jY0lchpkBAfnCKslzMkc";
+
+
 // Create a new component. This component should produce some html.
 const App = () => (
-    <div>Hola!</div>
+    <div>
+
+    </div>
 );
 
 // Take this component's generated HTML and put it on the page in the DOM
